057b2b31d3ededf859c6ec4d57d99a1ff440122f
hagiwa901 <kedama4156991@gmail.com>
2021-06-20 23:13:53 +0900

    add

---
+++ chapter2/main10.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+    "fmt"
+    "os"
+    "io/ioutil"
+	"strings"
+)
+
+func main() {
+	// ファイルをOpenする
+    f, err := os.Open("chapter2/hightemp.txt")
+    if err != nil{
+        fmt.Println("error")
+    }
+    defer f.Close()
+
+    // 一気に全部読み取り
+    b, err := ioutil.ReadAll(f)
+
+	// 行数
+	var column = 0
+
+	str := string(b)
+	slice := strings.Split(str, "\n")
+
+    // 出力
+	for _, s := range slice {
+		// 内容があるかどうか
+		if(len(s) > 0){
+			column++
+		}
+	}
+    fmt.Println(column)
+}
