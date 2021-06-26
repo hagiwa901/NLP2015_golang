7ab12633317ae8d9e8491ca6cbd0726af238dc31
hagiwa901 <kedama4156991@gmail.com>
2021-06-26 20:47:58 +0900

    add

---
+++ chapter2/main15.go
@@ -0,0 +1,38 @@
+/*
+自然数Nをコマンドライン引数などの手段で受け取り，入力のうち末尾のN行だけを表示せよ．
+確認にはtailコマンドを用いよ．
+*/
+
+package main
+
+import(
+    "fmt"
+    "os"
+    "io/ioutil"
+	"strings"
+	"flag"
+)
+
+func main() {
+	// コマンドライン引数
+    args := flag.Int("n", 24, "help message for n option")
+	flag.Parse()
+
+	// ファイルをOpenする
+    f, err := os.Open("chapter2/hightemp.txt")
+    if err != nil{
+        fmt.Println("error")
+    }
+    defer f.Close()
+
+    // 一気に全部読み取り
+    b, err := ioutil.ReadAll(f)
+
+	str := string(b)
+	slice := strings.Split(str, "\n")
+
+    // 出力
+	for i := *args-1; i >= 0; i-- {
+		fmt.Println(slice[i])
+	}
+}
